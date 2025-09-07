5b141da0a1dd8a73f0a9fe73e7b6553fda34be73
Sean Macdonald <47928514+seanmac5291@users.noreply.github.com>
2025-09-07 21:59:11 +0000

    Fix Netlify build: exclude netlify functions from TypeScript compilation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Exclude netlify functions from build
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Don't process netlify functions during build
+    config.module.rules.push({
+      test: /netlify\/functions/,
+      use: 'ignore-loader'
+    });
+    
+    return config;
+  },
 };
 
 export default nextConfig;
