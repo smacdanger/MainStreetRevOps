d3f1e75f94bd1b7809fc3244916288d0b8b5496b
Sean Macdonald <47928514+seanmac5291@users.noreply.github.com>
2025-09-07 22:03:16 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Exclude netlify functions from build
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Don't process netlify functions during build
-    config.module.rules.push({
-      test: /netlify\/functions/,
-      use: 'ignore-loader'
-    });
-    
-    return config;
-  },
+  // Simple config without webpack modifications
 };
 
 export default nextConfig;
